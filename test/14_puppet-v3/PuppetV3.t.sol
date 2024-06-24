f6c514c7dfbbbb80e802f1fb0100c0320cf9e046
EbiPenMan <karimi.gamedev@gmail.com>
2024-06-25 01:28:40 +0330

    fix all test

--- test/14_puppet-v3/PuppetV3.t.sol
+++ test/14_puppet-v3/PuppetV3.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import {ERC20} from "solmate/src/tokens/ERC20.sol";
 import "../../build-uniswap/v3/INonfungiblePositionManager.sol";
 
@@ -34,64 +35,80 @@ contract PuppetV3Test is Test {
     uint256 constant LENDING_POOL_INITIAL_TOKEN_BALANCE = 1000000e18;
 
     function setUp() public {
-        // Fork from mainnet state
-        string memory MAINNET_FORKING_URL = "";
-        vm.createFork(MAINNET_FORKING_URL);
-        vm.selectFork(0);
-        vm.rollFork(15450164);
-
-        // Initialize player account
-        player = vm.addr(2);
-        vm.deal(player, PLAYER_INITIAL_ETH_BALANCE);
-
         // Initialize deployer account
-        deployer = vm.addr(1);
+        // using private key of account #1 in Anvil's node
+        deployer = vm.addr(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
         vm.deal(deployer, DEPLOYER_INITIAL_ETH_BALANCE);
 
+        // Initialize player account
+        // using private key of account #2 in Anvil's node
+        player = vm.addr(0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d);
+        vm.deal(player, PLAYER_INITIAL_ETH_BALANCE);
+
         // Get a reference to the Uniswap V3 Factory contract
+        deployCodeTo("build-uniswap/v3/UniswapV3Factory.json", address(0x1F98431c8aD98523631AE4a59f267346ea31F984));
         uniswapFactory = IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
 
         // Get a reference to WETH9
+        vm.prank(address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
         weth = new WETH();
 
         // Deployer wraps ETH in WETH
+        vm.prank(deployer);
         weth.deposit{value: UNISWAP_INITIAL_WETH_LIQUIDITY}();
         assertEq(weth.balanceOf(deployer), UNISWAP_INITIAL_WETH_LIQUIDITY);
 
         // Deploy DVT token. This is the token to be traded against WETH in the Uniswap v3 pool.
+        vm.prank(deployer);
         token = new DamnValuableToken();
 
         // Create the Uniswap v3 pool
-        uniswapPositionManager = INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
+        bytes memory uniswapPositionManagerArgs = abi.encode(address(uniswapFactory), address(weth), address(0));
+        deployCodeTo(
+            "build-uniswap/v3/NonfungiblePositionManager.json",
+            uniswapPositionManagerArgs,
+            address(0xC36442b4a4522E871399CD717aBDD847Ab11FE88)
+        );
+        uniswapPositionManager = INonfungiblePositionManager(address(0xC36442b4a4522E871399CD717aBDD847Ab11FE88));
         uint24 FEE = 3000; // 0.3%
+        address token0 = address(weth);
+        address token1 = address(token);
+        (token0, token1) = token0 < token1 ? (token0, token1) : (token1, token0);
+
         uniswapPositionManager.createAndInitializePoolIfNecessary(
-            address(weth),  // token0
-            address(token), // token1
+            token0, // token0
+            token1, // token1
             FEE,
             encodePriceSqrt(1, 1)
         );
 
-        address uniswapPoolAddress = uniswapFactory.getPool(
-            address(weth),
-            address(token),
-            FEE
-        );
+        address uniswapPoolAddress = uniswapFactory.getPool(address(weth), address(token), FEE);
         uniswapPool = IUniswapV3Pool(uniswapPoolAddress);
         uniswapPool.increaseObservationCardinalityNext(40);
 
         // Deployer adds liquidity at current price to Uniswap V3 exchange
+
+
+        vm.startPrank(deployer);
         weth.approve(address(uniswapPositionManager), type(uint256).max);
         token.approve(address(uniswapPositionManager), type(uint256).max);
+
+        uint256 amount0Desired = UNISWAP_INITIAL_WETH_LIQUIDITY;
+        uint256 amount1Desired = UNISWAP_INITIAL_TOKEN_LIQUIDITY;
+
+        (amount0Desired, amount0Desired) =
+            token0 == address(weth) ? (amount0Desired, amount0Desired) : (amount0Desired, amount0Desired);
+
         uniswapPositionManager.mint(
             INonfungiblePositionManager.MintParams({
-                token0: address(weth),
-                token1: address(token),
+                token0: token0,
+                token1: token1,
                 tickLower: -60,
                 tickUpper: 60,
                 fee: FEE,
                 recipient: deployer,
-                amount0Desired: UNISWAP_INITIAL_WETH_LIQUIDITY,
-                amount1Desired: UNISWAP_INITIAL_TOKEN_LIQUIDITY,
+                amount0Desired: amount0Desired,
+                amount1Desired: amount1Desired,
                 amount0Min: 0,
                 amount1Min: 0,
                 deadline: block.timestamp * 2
@@ -129,14 +146,12 @@ contract PuppetV3Test is Test {
         assertLt(player.balance, LENDING_POOL_INITIAL_TOKEN_BALANCE * 3);
 
         initialBlockTimestamp = block.timestamp;
+        vm.stopPrank();
+
     }
 
     function encodePriceSqrt(uint256 reserve0, uint256 reserve1) internal pure returns (uint160) {
-        return uint160(
-            sqrt(
-                uint256(reserve1) * (2**192) / (reserve0)
-            )
-        );
+        return uint160(sqrt(uint256(reserve1) * (2 ** 192) / (reserve0)));
     }
 
     function sqrt(uint256 x) internal pure returns (uint256 y) {
@@ -162,7 +177,7 @@ contract PuppetV3Test is Test {
         // Block timestamp must not have changed too much
         // assertLt(block.timestamp - initialBlockTimestamp, 115, "Too much time passed");
 
-        // // Player has taken all tokens out of the pool        
+        // // Player has taken all tokens out of the pool
         // assertEq(
         //     token.balanceOf(address(lendingPool)),
         //     0
@@ -171,5 +186,30 @@ contract PuppetV3Test is Test {
         //     token.balanceOf(player),
         //     LENDING_POOL_INITIAL_TOKEN_BALANCE
         // );
+    }
+
+    function deployBytecodeWithArgs(string memory fileName, bytes memory constructorArgs)
+        public
+        returns (address contractAddress)
+    {
+        // Load the bytecode from JSON file
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/build-uniswap/v3/", fileName);
+        string memory json = vm.readFile(path);
+
+        // Parse bytecode
+        bytes memory bytecode = stdJson.readBytes(json, ".evm.bytecode.object");
+
+        // Combine bytecode and constructorArgs
+        bytes memory bytecodeWithArgs = abi.encodePacked(bytecode, constructorArgs);
+
+        assembly {
+            contractAddress := create(0, add(bytecode, 0x20), mload(bytecodeWithArgs))
+            // if iszero(extcodesize(contractAddress)) {
+            //     returndatacopy(0, 0, returndatasize())
+            //     revert(0, returndatasize())
+            // }
+        }
+        require(contractAddress != address(0), "Deployment failed");
     }
 }
